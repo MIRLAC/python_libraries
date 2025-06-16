52d62fea21eef6bdfba2c13c7324bb98a25242e2
MIRDULA C <165809137+MIRLAC@users.noreply.github.com>
2025-06-16 14:34:06 +0530

    dimensions

---
+++ dimensions.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array(13)
+b = np.array([13,3,6])
+c = np.array([[13,3,6],[14,10,9]])
+d = np.array([[[13,3,6],[14,10,9]],[[16,3,78],[2,10,75]]])
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
