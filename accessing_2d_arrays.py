66399412b0a1f3fdfeb0e2c2740b661716db9ede
MIRDULA C <165809137+MIRLAC@users.noreply.github.com>
2025-06-16 15:19:28 +0530

    modified the array

--- accessing_2d_arrays.py
+++ accessing_2d_arrays.py
@@ -1,3 +1,3 @@
 import numpy as np
-x = np.array([[13,3,6,],[14,10,9]])
-print("2 nd element on first row:",x[0,1])
+x = np.array([[13,3,6,16,3,78],[14,10,9,2,8,75]])
+print("5 th element in the second row:",x[1,5])
